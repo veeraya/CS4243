950de4629512609c47f6ed6033579ecb2fd6502c
Harshul Gandhi <gandhi.harshul.88@gmail.com>
2014-10-24 23:45:38 +0800

    Added functionality to get pixel value on click

--- ImageLoader_3.py
+++ ImageLoader_3.py
@@ -3,16 +3,15 @@ from PyQt4 import QtCore, QtGui, uic
  
 form_class = uic.loadUiType("ImageLoader.ui")[0]
 
-
+x = 0.0
+y = 0.0
 class ImageLoad(QtGui.QMainWindow, form_class):
 	def __init__(self, parent=None):
 		QtGui.QMainWindow.__init__(self, parent)
 	        self.setupUi(self)
 		self.loadImg_btn.clicked.connect(self.loadImageOnClick)
 		self.label_2.setVisible(False)
-		image = QLabel()
-		self.image.setObjectName("image")
-		self.image.mousePressEvent = self.getPos
+		self.label_5.setVisible(False)
 
 
 	def loadImageOnUI(self,filename):
@@ -23,18 +22,34 @@ class ImageLoad(QtGui.QMainWindow, form_class):
 #loadImg_btn
 
 	def loadImageOnClick(self):
+		global x
+		global y
 		filename = QtGui.QFileDialog.getOpenFileName(self, 'Open File', '.')
 
 		self.label.setScaledContents(True);
  		self.label.setPixmap(self.loadImageOnUI(filename))
+
+#		image = QLabel()
+#		self.image.setPixmap
+#		self.image.setObjectName("image")
+		self.label.mousePressEvent = self.getPos
+#		self.getPos(self.label.mousePressEvent)
+		#self.label.mousePressEvent(self.getPos)
+		
 		self.labelWidth.setText(str(self.loadImageOnUI(filename).height()))
 		self.label_2.setVisible(True)
 		self.labelHeight.setText(str(self.loadImageOnUI(filename).width()))
-		self.image.setPixmap(QPixmap(filename))
+		#self.image.setPixmap(QPixmap(filename))
 
 	def getPos(self , event):
-	    x = event.pos().x()
-	    y = event.pos().y() 
+		global x
+		global y
+		self.label_5.setVisible(True)
+		self.label_x.setText(str(event.pos().x()))
+		self.label_y.setText(str(event.pos().y()))
+#		x = 
+#		y = 
+	
 
 		
 
