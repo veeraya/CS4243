fcf7eee591a59999a2383d76e7940633d74208df
Veeraya Pupatwibul <veeraya.pupat@gmail.com>
2014-11-14 03:19:33 +0800

    code for multi-threading (disable by default)

--- projection/multi_depth.py
+++ projection/multi_depth.py
@@ -8,6 +8,7 @@ import cv2
 import math
 import sys
 import pickle
+from multiprocessing import Process
 
 # GLOBAL Camera intrinsic params
 u0 = 0
@@ -41,7 +42,7 @@ def main():
     cam_position = get_cam_position(angle=math.radians(-degree * 2.0 / no_frames), no_frames=10, cam_original_position=cam_original_position, k=2)
     cam_orientation = get_cam_orientation(angle=math.radians(degree * 2.0 * scale / no_frames), no_frames=10, cam_original_orientation = cam_original_orientation)
 
-    project_and_draw(pts1, cam_position, cam_orientation, 0, 10)
+    project_and_draw(pts1, cam_position, cam_orientation, 0, 10, multithread=False)
 
 def draw_image(pts, filename = "frame.png", use_cloud = False):
     """
@@ -83,7 +84,7 @@ def draw_image(pts, filename = "frame.png", use_cloud = False):
     cv2.imwrite(filename ,projection_matrix)
 
 
-def project_and_draw(pts, cam_position, cam_orientation, start_frame, end_frame):
+def project_and_draw(pts, cam_position, cam_orientation, start_frame, end_frame, multithread = False):
     """
     :param pts: list of points
     :param cam_position: camera position
@@ -98,30 +99,41 @@ def project_and_draw(pts, cam_position, cam_orientation, start_frame, end_frame)
     pts[x,y,2] = (x, y) of projected points
     """
     for frame in range(start_frame, end_frame+1):
-        new_pts = np.array(pts, copy=True)
-        depth_array = np.empty((pts.shape[0], pts.shape[1]))
-        depth_array[:,:] = sys.maxint
-        tf = np.matrix(cam_position[frame][1:]).reshape(3,1)
-        i_f = np.matrix(cam_orientation[frame][:][0]).reshape(3,1)
-        j_f = np.matrix(cam_orientation[frame][:][1]).reshape(3,1)
-        k_f = np.matrix(cam_orientation[frame][:][2]).reshape(3,1)
-        for x in range(0, pts.shape[0]):
-            for y in range(0, pts.shape[1]):
-                if type(pts[x,y,0]) == tuple or type(pts[x,y,0]) == list:
-                    for z in pts[x,y,0]:
-                        # - 816 because we want to shift our image such that the midpoint has x value of 0
-                        sp = np.matrix([[x-816],[y],[z]])
-                        u_fp = ((f * np.transpose((sp - tf)) * i_f * Bu) / (np.transpose((sp - tf)) * k_f)) + u0
-                        v_fp = ((f * np.transpose((sp - tf)) * j_f * Bv) / (np.transpose((sp - tf)) * k_f)) + v0
-                        x_projected = u_fp[0,0]
-                        y_projected = v_fp[0,0]
-                        if type(new_pts[x,y,2]) == list:
-                            new_pts[x,y,2].append((x_projected, y_projected))
-                        else:
-                            new_pts[x,y,2] = [(x_projected, y_projected)]
-        draw_image(new_pts, "frame_%d.png" % frame)
-        print "Save frame_%d.png" % frame
+        if multithread:
+            p = Process(target=project_and_draw_single_frame, args=(pts, cam_position, cam_orientation, frame))
+            p.start()
+        else:
+            project_and_draw_single_frame(pts, cam_position, cam_orientation, frame)
 
+def project_and_draw_single_frame(pts, cam_position, cam_orientation, frame):
+    """
+    Project and draw single frame. This used to be part of the function project_and_draw but its has been
+    made into a separate function to enable multi-threading.
+    """
+    print "Starting project and draw frame %d" % frame
+    new_pts = np.array(pts, copy=True)
+    depth_array = np.empty((pts.shape[0], pts.shape[1]))
+    depth_array[:,:] = sys.maxint
+    tf = np.matrix(cam_position[frame][1:]).reshape(3,1)
+    i_f = np.matrix(cam_orientation[frame][:][0]).reshape(3,1)
+    j_f = np.matrix(cam_orientation[frame][:][1]).reshape(3,1)
+    k_f = np.matrix(cam_orientation[frame][:][2]).reshape(3,1)
+    for x in range(0, pts.shape[0]):
+        for y in range(0, pts.shape[1]):
+            if type(pts[x,y,0]) == tuple or type(pts[x,y,0]) == list:
+                for z in pts[x,y,0]:
+                    # - 816 because we want to shift our image such that the midpoint has x value of 0
+                    sp = np.matrix([[x-816],[y],[z]])
+                    u_fp = ((f * np.transpose((sp - tf)) * i_f * Bu) / (np.transpose((sp - tf)) * k_f)) + u0
+                    v_fp = ((f * np.transpose((sp - tf)) * j_f * Bv) / (np.transpose((sp - tf)) * k_f)) + v0
+                    x_projected = u_fp[0,0]
+                    y_projected = v_fp[0,0]
+                    if type(new_pts[x,y,2]) == list:
+                        new_pts[x,y,2].append((x_projected, y_projected))
+                    else:
+                        new_pts[x,y,2] = [(x_projected, y_projected)]
+    draw_image(new_pts, "frame_%d.png" % frame)
+    print "Save frame_%d.png" % frame
 
 def get_cam_position(angle = -np.pi/6, no_frames = 12, cam_original_position = [0,0,100,-5 * 100], k = 2):
     """
